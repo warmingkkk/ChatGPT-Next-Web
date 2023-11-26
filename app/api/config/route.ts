17ca156ea9232d40f223a9436c482f03f0e0b7f2
warmingkkk <1071059972@qq.com>
2023-11-26 19:33:55 +0800

    Update route.ts

--- app/api/config/route.ts
+++ app/api/config/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 import { getServerSideConfig } from "../../config/server";
 
@@ -19,7 +19,15 @@ declare global {
   type DangerConfig = typeof DANGER_CONFIG;
 }
 
-async function handle() {
+// List of disabled regions
+const DISABLED_REGIONS = ['HKG1'];
+
+async function handle(request: NextRequest) {
+  // Check if the region is disabled
+  if (DISABLED_REGIONS.includes(request.cf.colo)) {
+    return new NextResponse('Service not available in your region', { status: 403 });
+  }
+
   return NextResponse.json(DANGER_CONFIG);
 }
 
